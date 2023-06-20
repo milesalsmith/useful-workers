82ff615d1a05da0916a07f7c93969399bcc1cf6e
milessmith <miles@cloudflare.com>
2023-06-20 16:05:23 +0100

    updating china-redirect worker.

--- china-redirect.js
+++ china-redirect.js
@@ -1,21 +1,17 @@
+async function handleRequest(request) {
+  const language = request.headers.get("Accept-Language")
+  const requestUrl = new URL(request.url)
+  const url = requestUrl.toString()
+                 .replace("electricjellyfish.org","electricjellyfish.org/cn")
+
+if (language.includes('zh')) {
+  return Response.redirect(url, 301)
+} else {
+    return new Response("mlem", { status: 403})  
+  } 
+  return await fetch(request)
+}
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-
-async function handleRequest(request) {
-  const acceptLanguage = request.headers.get('accept-language')
-
-  // Check if the 'accept-language' header includes simplified Chinese (zh-CN)
-  if (acceptLanguage && acceptLanguage.includes('zh-CN')) {
-    const url = new URL(request.url)
-
-    // Redirect to 'electricjellyfish.org/secure'
-    url.pathname = '/secure'
-
-    return Response.redirect(url.toString(), 302)
-  }
-
-  // Pass the request through without redirection
-  return fetch(request)
-}
-
